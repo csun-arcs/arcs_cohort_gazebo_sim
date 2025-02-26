db895bd7a8f55e7ffd108e3ff9497deb1b74314b
pikaic <pikaic2010@gmail.com>
2025-02-26 12:20:35 -0800

    added diff drive control for gazebo sim

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -50,10 +50,41 @@ def generate_launch_description():
                                    '-z', '0.1'],
                         output='screen')
 
+    bridge_params = os.path.join(
+        get_package_share_directory(package_name_gazebo_sim),
+        'config',
+        'gazebo_bridge.yaml'
+    )
+
+    start_gazebo_ros_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+
+    ros_gz_image_bridge_left = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/left_camera_image_raw"]
+    )
+
+    ros_gz_image_bridge_right = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/right_camera_image_raw"]
+    )
 
     return LaunchDescription([
         rsp,
         world_arg,
         gazebo,
         spawn_entity,
+        start_gazebo_ros_bridge,
+        ros_gz_image_bridge_left,
+        ros_gz_image_bridge_right
     ])
