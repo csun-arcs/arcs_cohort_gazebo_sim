4e164a93ace1bd53fbf91f0980b449c725a5d858
Barry Ridge <barry.ridge@csun.edu>
2025-03-06 11:59:01 -0800

    Improve robot_name description

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -53,7 +53,9 @@ def generate_launch_description():
     declare_robot_name_cmd = DeclareLaunchArgument(
         'robot_name',
         default_value='',
-        description='Name of the robot (used to form prefixes in robot model description).'
+        description=(
+            'Name of the robot (specifying this will add the '
+            'robot name prefix to joints, links, etc. in the robot model).')
     )
     # NOTE: Removing the namespace parameter from the Gazebo launcher for now
     # due to double-namespacing issue when launcher is called from an upstream
